b7ae47917ddb6a385137c1e82741d9f38cfd48c8
Luccas Correa <luccascorrea@estudio89.com.br>
2017-08-31 00:56:06 -0300

    Improved log messages.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='e89-syncing',
-    version='2.0.1',
+    version='2.0.2',
     packages=['e89_syncing', 'e89_syncing.migrations'],
     include_package_data=True,
     license='BSD License',  # example license
